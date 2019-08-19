35bd0dba9c3cb1cfd14e075b40cb1d0eaf148bc9
Daviel <davielsanchez@gmail.com>
2019-08-19 21:38:13 +0200

    Test branch permision 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const checkJwt = jwt({
   // Dynamically provide a signing key based on the kid in the header
   // and the signing keys provided by the JWKS endpoint.
   secret: jwksRsa.expressJwtSecret({
-    cache: true, // cache the signing key
+    cache: true, // cache the signing key 
     rateLimit: true,
     jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
     jwksUri: `https://${
